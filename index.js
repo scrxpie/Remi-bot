9a63811412ca4aeebd244be217190d77e62dc62e
scrxpie <elsenn3345@gmail.com>
2025-06-26 16:11:33 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config(); // EN ÜSTE KOY
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.commands = new Collection();
+
+// Komutları yükle
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+// Eventleri yükle
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// MongoDB bağlantısı
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("✅ MongoDB'ye bağlandı!");
+}).catch(err => {
+  console.error("❌ MongoDB bağlantı hatası:", err);
+});
+
+// Botu başlat
+client.login(process.env.TOKEN);
